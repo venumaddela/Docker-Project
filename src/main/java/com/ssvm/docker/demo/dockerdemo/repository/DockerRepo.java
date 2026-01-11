203fa877b9ecfb23bb837f11ef0c02066f5d830a
venumaddela <66516138+venumaddela@users.noreply.github.com>
2026-01-12 00:21:01 +0530

    Update DockerRepo.java

--- src/main/java/com/ssvm/docker/demo/dockerdemo/repository/DockerRepo.java
+++ src/main/java/com/ssvm/docker/demo/dockerdemo/repository/DockerRepo.java
@@ -1,7 +1,7 @@
-package com.ssvm.docker.demo.dockerdemo.repository;
+/*package com.ssvm.docker.demo.dockerdemo.repository;
 
 import com.ssvm.docker.demo.dockerdemo.entity.DockerRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DockerRepo extends JpaRepository<DockerRequest,Long> {
-}
+}*/
